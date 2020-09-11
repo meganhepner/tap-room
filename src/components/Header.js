3130f4a1a9230f44e7e830101cb6da35f75b61c2
Megan Hepner <megan.hepner@gmail.com>
2020-09-11 08:56:58 -0700

    create Header.js & add to App.js, create boilerplate code in KegControl

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header(){
+  return(
+    <h1>
+      Megan's Tap Room
+    </h1>
+  );
+}
+
+export default Header;
+
---
+++ src/components/KegControl.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class KegControl extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+
+      };
+    }
+
+    render(){
+      
+      
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+
+    }
+
+}
+
+export default KegControl;
